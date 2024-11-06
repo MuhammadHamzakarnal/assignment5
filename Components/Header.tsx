db46474449bf17cdd99614713e6fe7bd5a9f5a65
MuhammadHamzakarnal <0310muhammadhamza356@gmail.com>
2024-11-06 12:21:23 -0800

    third commit

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -1,16 +1,17 @@
 import React from "react";
-import {Rye} from "next/font/google"
-const rye = Rye ({
-  weight:'400',
-  subsets:["latin"]},
-  
-)
+import { Rye } from "next/font/google";
 
+const rye = Rye({
+  weight: '400',
+  subsets: ["latin"]
+});
 
 const Header = () => {
   return (
-    <div className="bg-[#A29875] h-[134px]">
-      <h1 className={`${rye.className} text-[75px] text-[#FFFFFF] font-normal`}>MANZZARI</h1>
+    <div style={{ height: '134px' }} className="bg-[#A29875]">
+      <h1 className={`${rye.className} text-[75px] text-[#FFFFFF] font-normal`}>
+        MANZZARI
+      </h1>
     </div>
   );
 };
