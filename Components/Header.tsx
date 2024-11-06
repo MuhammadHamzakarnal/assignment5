7c680fdd14eb44229a040bb9f471b0d8731ce084
MuhammadHamzakarnal <0310muhammadhamza356@gmail.com>
2024-11-06 12:42:39 -0800

    sixth commit

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -8,8 +8,11 @@ const rye = Rye({
 
 const Header = () => {
   return (
-    <div className="h-[134px] bg-[#A29875]">
-      <h1 className={`${rye.className} text-[75px] text-[#FFFFFF] font-normal`}>
+    <div style={{ height: '134px' }} className="bg-[#A29875]">
+      <h1
+        className={`${rye.className} text-[#FFFFFF] font-normal`}
+        style={{ fontSize: '75px' }} 
+      >
         MANZZARI
       </h1>
     </div>
