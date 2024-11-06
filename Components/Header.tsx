7246bb3bd56177298552bfdc120f5219b0e4058d
MuhammadHamzakarnal <0310muhammadhamza356@gmail.com>
2024-11-06 12:16:50 -0800

    height commit

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -9,7 +9,7 @@ const rye = Rye ({
 
 const Header = () => {
   return (
-    <div className="bg-[#A29875] h-96 ">
+    <div className="bg-[#A29875] h-[134px]">
       <h1 className={`${rye.className} text-[75px] text-[#FFFFFF] font-normal`}>MANZZARI</h1>
     </div>
   );
