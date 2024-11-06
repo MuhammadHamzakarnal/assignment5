f0ea7effc58311f2577a0b85bf5e2d5b0bc35ddf
MuhammadHamzakarnal <0310muhammadhamza356@gmail.com>
2024-11-06 12:04:43 -0800

    header file change

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -9,7 +9,7 @@ const rye = Rye ({
 
 const Header = () => {
   return (
-    <div className="bg-[#A29875] h-[134px]">
+    <div className="bg-[#A29875] ">
       <h1 className={`${rye.className} text-[75px] text-[#FFFFFF] font-normal`}>MANZZARI</h1>
     </div>
   );
